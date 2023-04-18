a8d59079b8d42fe17156bebb60b0e37bc2e903fb
lhpark114 <75491899+lhpark114@users.noreply.github.com>
2023-04-18 20:04:01 +0000

    AddTodo + UUID

---
+++ src/components/AddTodo.jsx
@@ -0,0 +1,33 @@
+import React, {useState} from 'react'
+import { v4 as uuidv4 } from 'uuid';
+
+export default function AddTodo({ onAdd }){
+  const [task, setTask] = useState('');
+  const handleChange = (e) => {
+    setTask(e.target.value);
+  } 
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(task.trim.length === 0) {
+      return;
+    }
+    onAdd({id: uuid4(), work, status: 'active'});
+    setTask('');
+  };
+  
+  return(
+    <form onSubmit = {handleSubmit} >
+      <input 
+        type='text'
+        placeholder='Input Todos'
+        value = {task}
+        onChange = {handleChange}      
+        />
+        <button>
+          Add
+        </button>
+    </form>
+  );
+  
+}
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import React, {useState} from 'react'
+import AddTodo from './AddTodo'
 
 export default function TodoList(){
+  const [todos, setTodos] = useState([
+    {id:1,
+    work: '투두',
+    status: 'done'
+    },
+
+    {id:12,
+    work: 'Next 강의',
+    status: 'done'
+    },
+  ]);
+
+  const handleAdd = (todo) => setTodos([...todos, todo]);
+
   return (
+    <section>
+      <ul>
+        {todos.map((todo) => {
+        (
+          <li key={todo.id}>{todo.work}</li>
+        )
+        })}
+    </ul>
+    <AddTodo onAdd={handleAdd}/>
     
-  )
+    </section>
+  );
 }
