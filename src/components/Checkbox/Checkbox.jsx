3219fe721257a3702322b2d8b0907bff3fab2629
lhpark114 <75491899+lhpark114@users.noreply.github.com>
2023-04-20 02:05:02 +0000

    checkbox + delete

---
+++ src/components/Checkbox/Checkbox.jsx
@@ -0,0 +1,22 @@
+import react, {useState} from 'react';
+
+export default function Checkbox({onCheck}) {
+  const [checked, setChecked] = useState(false);
+  const handleChange = () => {
+    setChecked(!checked);
+    onCheck({status:'done'});
+  };
+  return (
+    <>
+      <label htmlFor='checkbox'>
+        </label>
+       <input 
+         id='checkbox'
+         type='checkbox'
+         value={checked}
+         onChange={handleChange} 
+       />
+      
+    </>
+  );
+}
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,5 +1,7 @@
 import React, {useState} from 'react'
 import AddTodo from '../AddTodo/AddTodo'
+import Checkbox from '../Checkbox/Checkbox'
+import { MdOutlineCancel } from 'react-icons/md';
 
 export default function TodoList(){
   const [todos, setTodos] = useState([
@@ -14,18 +16,26 @@ export default function TodoList(){
     },
   ]);
 
+  
   const handleAdd = (todo) => setTodos([...todos, todo]);
-
+  const handleDelete = (id) => {
+    setTodos(todos.filter((item) => item.id !== id))};
+  const handleCheck = (todo) => setTodos([...todos, todos.status: 'done'])
+  
   return (
     <section>
       <ul>
         {
-          todos.map((item) => 
-          <li key={item.id}>{item.task}</li>
+          todos.map((item) => (
+              <li key={item.id}> 
+                <Checkbox onCheck={handleCheck}/> {item.task} 
+                <button onClick={handleDelete}> <MdOutlineCancel />
+                </button>
+              </li>)
         )}
+         
     </ul>
     <AddTodo onAdd={handleAdd}/>
-    
     </section>
   );
 }
